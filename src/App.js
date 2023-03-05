f7093c9821825bdf02eaf1d868764e4ff0604bb3
asdhamidi <asad.hamidi119@gmail.com>
2023-03-05 18:31:17 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [ans, setAns] = useState(0);
+  const [present, setPresent] = useState(0);
+  const [total, setTotal] = useState(null);
+
+  function calc() {
+    let p = present,
+      t = total,
+      ans = 0;
+
+    while ((p / t) * 100 < 75) {
+      p++;
+      t++;
+      ans++;
+    }
+
+    setAns(ans);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="title">
+        <h1>Attendance Headache</h1>
+        <h2 className="subtitle">Count days till 75%</h2>
       </header>
+      <main>
+        <label>
+          Days Present
+          <input
+            field="present"
+            onChange={(e) => setPresent(e.target.value)}
+            type="number"
+          />
+        </label>
+        <label>
+          Total Days
+          <input
+            field="total"
+            onChange={(e) => setTotal(e.target.value)}
+            type="number"
+          />
+        </label>
+        <button onClick={calc}>Calculate</button>
+        <div className="result">
+          {ans !== null &&
+            "You need to attend " + ans + " more days to reach 75%."}
+        </div>
+      </main>
     </div>
   );
 }
